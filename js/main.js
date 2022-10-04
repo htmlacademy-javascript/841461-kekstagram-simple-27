feda73cad55fa9fa6052569f30758b8ae7a1d6cb
Gerda <gerda0283@gmail.com>
2022-10-04 17:07:46 +0200

    Внесение правок в module2-task1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
 /* developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/randomath.random The maximum is inclusive and the minimum is inclusive */
 function getRandomIntInclusive(min, max) {
-  min = Math.abs(Math.ceil(min));
-  max = Math.abs(Math.floor(max));
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 getRandomIntInclusive();
 
-/* At first we get quantity of symbols of current coment */
-function getCurrentStringLength(currentString) {
-
+/* The second: we check validity of quantity of symbols */
+function isStringLengthValid(currentString, maxComentLength) {
   const stringArr = Array.from(currentString);
   const stringLength = stringArr.length;
-
-  return stringLength;
+  return  stringLength <= maxComentLength;
 }
-const currentComentLength = getCurrentStringLength();
-
-/* The second: we check validity of quantity of symbols */
-function isStringLengthValid(maxComentLength) {
-  return currentComentLength <= maxComentLength ? true : false;
-}
-const validComent = isStringLengthValid();
+isStringLengthValid();
